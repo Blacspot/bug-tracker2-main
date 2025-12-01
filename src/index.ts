5f961429b55f9595e450c09cc979fa81e9827288
blacspot <obwogemcbride73@gmail.com>
2025-12-01 11:26:55 +0300

    the preflight fix

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use(cors({
   credentials: true,
 }));
 
-app.options('*', cors()); // Enable pre-flight for all routes
+app.options("(.*)", cors()); // Enable pre-flight for all routes
 
 
 // Middleware
